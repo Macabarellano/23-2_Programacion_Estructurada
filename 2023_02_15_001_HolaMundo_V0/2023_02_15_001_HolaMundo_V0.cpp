df240f7786b71f32b1ce2b00cac60378eff9bf43
Macabarellano <macabarellano@gmail.com>
2023-02-15 08:42:39 -0600

    001_Mau
    Creación del primer programa llamado hola mundo

---
+++ 2023_02_15_001_HolaMundo_V0/2023_02_15_001_HolaMundo_V0.cpp
@@ -0,0 +1,12 @@
+// 2023_02_15_001_HolaMundo_V0.cpp : 
+// Este archivo es mi primer programa 
+// Mauricio Cabrera 0012318553241
+
+
+//En este apartado se agregan todas las bibliotecas necesarias
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
